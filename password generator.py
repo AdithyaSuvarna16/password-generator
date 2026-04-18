c8c2255a21ef07102a42b617975e95a11778c74b
AdithyaSuvarna16 <adithyasuvarna209@gmail.com>
2026-04-18 16:15:40 +0530

    Added Password Generator Script

---
+++ password generator.py
@@ -0,0 +1,16 @@
+import random
+import string
+def password_generator(length):
+    letters=string.ascii_letters
+    digits=string.digits
+    character=string.punctuation
+    all_character=letters+digits+character
+    password=''.join(random.choice(all_character)for i in range(length))
+    return password
+    
+length=int(input("Enter the length of the password:"))  
+if length < 6:
+        print("Password should be at least 6 characters")
+else:
+    password = password_generator(length)
+    print("Generated Password:", password)
